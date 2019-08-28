d6614128708f3c7330ffe7755ec77bdeee8b2e1d
Lucas Magasweran <lucasrangit@gmail.com>
2019-08-28 11:51:27 +0200

    start.sh: only start IDE if provisioning succeeded

--- kinetis-design-studio/start.sh
+++ kinetis-design-studio/start.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 #
-# Start the development environment
+# Start the IDE and provision VM if necessary
 #
-
-vagrant up
-vagrant ssh -c "SWT_GTK3=0 UBUNTU_MENUPROXY=0 kinetis-design-studio -data /home/vagrant/workspace"
-
+vagrant up && \
+vagrant ssh -c "SWT_GTK3=0 UBUNTU_MENUPROXY=0 kinetis-design-studio -data /home/vagrant/workspace" &
--- mcuxpresso/start.sh
+++ mcuxpresso/start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
-vagrant up
+#
+# Start the IDE and provision IDE if necessary
+#
+vagrant up && \
 vagrant ssh -c "SWT_GTK3=0 UBUNTU_MENUPROXY=0 mcuxpressoide -data /home/vagrant/workspace" &
 
